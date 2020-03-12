e352d13170a3bee16ba5459a1fbcdd8f2937fc9d
lauravillalba <lvillalba@live.com>
2020-03-12 17:34:52 +0100

    freq modulada error

--- src/transcription.py
+++ src/transcription.py
@@ -3,13 +3,14 @@ from os import path
 from pydub import AudioSegment
 
 # convert mp3 file to wav                                                       
-sound = AudioSegment.from_file("../src/transcription3.aiff", format = 'aiff')
-sound.export("transcript.wav", format="wav", parameters=["-ar", "8000"])
+sound = AudioSegment.from_file("../outputs/selection/2033_0.mp3", format = 'mp3')
+sound.set_frame_rate(8000)
+sound.export("../outputs/transcript/transcript.wav", format="wav")
 #sound = AudioSegment.from_file("transcript.wav", format="wav", frame_rate=16000)
 #sound.export("transcript.wav", format="wav", parameters=["-ar", "8000"])
 
 # transcribe audio file                                                         
-AUDIO_FILE = "transcript.wav"
+AUDIO_FILE = "../outputs/transcript/transcript.wav"
 
 # use the audio file as the audio source                                        
 r = sr.Recognizer()
