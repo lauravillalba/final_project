382e39d3f31ed169955aa57657524bc472e16837
lauravillalba <lvillalba@live.com>
2020-03-13 03:06:50 +0100

    api ⭐️

--- src/api.py
+++ src/api.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, flash, request, redirect, url_for, send_from_directory, jsonify
+from flask import Flask, flash, request, redirect, url_for, send_from_directory, jsonify, render_template
 from werkzeug.utils import secure_filename
 from predictions import predictAudio, featuresFFT
 import glob
@@ -16,47 +16,15 @@ def allowed_file(filename):
 
 @app.route('/')
 def inicio():
-    return 
+    return render_template("index.html")
 
+@app.route('/upload')
+def upload():
+    return render_template("upload.html")
 
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
-
-@app.route('/uploadAudio', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            to = '/speakers/'+ filename
-            return redirect(to)
-            
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
-
-@app.route('/speakers/<audioname>', methods=['GET'])
-def speakersInAudio(audioname):
-    #audioName => audio_mix.mp3
-    path = f"../outputs/audioMix/{audioname}"
+@app.route('/speakers', methods=['GET','POST'])
+def olakase():
+    path = f"../outputs/audioMix/{request.files['file'].filename}"
     X, speech = featuresFFT(path)
     allNames, speakers_names, speech = predictAudio(X, speech)
     
@@ -66,6 +34,7 @@ def speakersInAudio(audioname):
     resum = speakers_names
     print("speakers_names: ",speakers_names)
 
-    return jsonify(resum,subtitles)
+    print(request.files)
+    return render_template('result.html', data=subtitles)
 
 app.run('0.0.0.0', port=5000, debug=False, threaded=False)
